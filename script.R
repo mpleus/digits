fc29c2735508b50f5a15cecb2747e5ebc0e56a7e
Milan Pleus <milan.pleus@gmail.com>
2018-12-08 15:54:16 +0100

    Logit schatten

--- script.R
+++ script.R
@@ -1,3 +1,4 @@
+# Set working directory
 user <- Sys.getenv("USER")
 
 if (user == "rutgerpoldermans"){
@@ -8,5 +9,16 @@ if (user == "rutgerpoldermans"){
   break
 }
 
+# Import data
 traindata <- read.csv("train.csv")
 testdata <- read.csv("test.csv")
+
+traindata$zero <- (traindata$label == 0)
+namesX <- colnames(traindata)
+namesX <-namesX[grep("^[pP].*", namesX)]  
+namesX <- paste(namesX,collapse="+")
+
+# Estimate logit for zero's
+fit <- glm(as.formula(paste("zero ~ ", namesX, sep = "")), data = traindata, family = "binomial")
+summary(fit)
+predict(fit)
