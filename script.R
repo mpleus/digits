a1fa0fe0151a03ae853c9e8998dccb3ee434dd4e
rutgerpoldermans <rwpoldermans@hotmail.com>
2018-12-09 02:40:07 +0100

    basic confusion and digit plot

--- script.R
+++ script.R
@@ -33,9 +33,23 @@ fitted <-predict(fit)
 probs <- (exp(fitted)/(1+exp(fitted))>0.5)
 traindata$probs <- probs
 
+# Very basic confusion matrix
+table(true=as.numeric(traindata$zero), pred=as.numeric(traindata$probs))
+
 # Plot histogram 
 if (user == "rutgerpoldermans"){
-hist((exp(fitted)/(1+exp(fitted))))
+  hist((exp(fitted)/(1+exp(fitted))))
+  #select a random row of traindata for visualization
+  temp_namesX <- colnames(traindata)
+  temp_namesX <- temp_namesX[temp_namesX %in% grep("pixel", temp_namesX, value=T)]
+  row <- as.numeric(traindata[sample(1:dim(traindata)[1],1), temp_namesX])
+  
+  # reshaping is required for image function 
+  m <- t(apply(matrix(row, byrow = T, nrow = 28, ncol = 28), 2, rev))
+  x <- 10*1:nrow(m); y <- 10*1:ncol(m)
+  image(x,y, m, axes = T, col = gray(seq(0,1, length=200)))
+  # a better better look
+  # filled.contour(x,y,m, color = topo.colors, asp = 1)
 }
 
 # Logit requires 10 different models
